9289dcaa2e92b139fd403363f35f1a2514c5ac85
Katie Siegel <ksiegel@mit.edu>
2016-11-27 18:49:43 -0800

    helpers/util.go: Add methods for converting between time and ms

--- helpers/util.go
+++ helpers/util.go
@@ -13,3 +13,16 @@ func PanicIfError(err error) {
 func TimeToMs(t time.Time) int64 {
 	return t.UnixNano() / int64(time.Millisecond)
 }
+
+// MsToTime converts a timestamp in ms to a time object.
+func MsToTime(timeMs int64) time.Time {
+	return time.Unix(0, timeMs*int64(time.Millisecond))
+}
+
+// TimeNowInMs returns the current time in milliseconds.
+func TimeNowInMs() int64 {
+	return TimeToMs(time.Now())
+}
+
+const DurationDayMs = int64(24 * 60 * 60 * 1000)
+const DurationWeekMs = DurationDayMs * 7
